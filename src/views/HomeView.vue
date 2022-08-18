bf8cd729b6f441f0fcfe0f2f2698102aa2254036
m1ch93l <91449532+m1ch93l@users.noreply.github.com>
2022-08-18 17:29:58 +0800

    added portrait view divider

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <HelloWorld />
+  <PortraitViewDivider />
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import HelloWorld from '../components/HelloWorld.vue'
+import PortraitViewDivider from '../components/PortraitViewDivider.vue'
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
+    HelloWorld, PortraitViewDivider,
   },
 });
 </script>
