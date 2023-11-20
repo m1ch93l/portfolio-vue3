6e622b91e275c72402b4e1845e2200d36aed695e
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2023-11-20 19:40:08 +0800

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <v-container>
       <div class="v-card text">
-        <p class="font-weight-bold mb-0 text-h3">
+        <p class="font-weight-bold mb-4 text-h3">
           Passionate Full-Stack Developer with Expertise in Vue.js, PHP Laravel, and React.js<br>
         </p>
-        <p class="font-weight-bold mb-0 text-h4">
+        <p class="mb-4 text-h5">
           Hello, I'm Michael, a dynamic and results-driven Full-Stack Developer with a strong foundation in Vue.js, PHP Laravel, and React.js. With a keen eye for detail and a commitment to creating seamless user experiences, I bring innovation and efficiency to every project.<br>
           <b>Technical Proficiency:</b><br>
         </p>
-        <p class="font-weight-bold mb-0 text-h4">
+        <p class="mb-4 text-h5">
           PHP Laravel: With a solid grasp of PHP Laravel, I excel in developing robust and scalable back-end solutions. My proficiency in Laravel ensures the creation of secure and efficient server-side applications, delivering seamless performance and optimal functionality.
         </p>
       </div>
