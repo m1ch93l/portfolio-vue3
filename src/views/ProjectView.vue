7f0e07f3eadc45548928b43576cb584bf5d0b69d
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-03-19 01:00:23 +0800

    Create a grid view.

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,21 +1,49 @@
 <template>
-  <ProjectTitle/>
-  <CardCom />
+<ProjectTitle />
+
+<v-container>
+    <v-row no-gutters>
+        <v-col cols="12" sm="4">
+            <v-sheet class="ma-2 pa-2">
+                <Project1 />
+            </v-sheet>
+        </v-col>
+        <v-col cols="12" sm="4">
+            <v-sheet class="ma-2 pa-2">
+                <Project2 />
+            </v-sheet>
+        </v-col>
+        <v-col cols="12" sm="4">
+            <v-sheet class="ma-2 pa-2">
+                <Project3 />
+            </v-sheet>
+        </v-col>
+    </v-row>
+</v-container>
+
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import {
+    defineComponent
+} from 'vue';
 
 // Components
-import CardCom from '../components/CardCom.vue'
+//import CardCom from '../components/CardCom.vue'
 import ProjectTitle from '@/components/ProjectTitle.vue';
+import Project1 from '@/projects/Project_1.vue';
+import Project2 from '@/projects/Project_2.vue';
+import Project3 from '@/projects/Project_3.vue';
 
 export default defineComponent({
-  name: 'ProjectView',
+    name: 'ProjectView',
 
-  components: {
-    CardCom,
-    ProjectTitle
-},
+    components: {
+        //CardCom,
+        ProjectTitle,
+        Project1,
+        Project2,
+        Project3
+    },
 });
 </script>
